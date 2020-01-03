a0e022b01c42c2a673e486b5500b2c960dab07a5
Devair Luiz Moitim <dmoitim@gmail.com>
2020-01-03 12:06:34 -0300

    Test refactor, subtests and when function is called with an empty string

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Devair")
-	want := "Hello, Devair"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := Hello("Devair")
+		want := "Hello, Devair"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
 }
